a89d62a7d20634f7996cf4c1dfac7580b49f4843
Crazyht <crazyht@codes-sources.com>
2012-11-27 19:51:51 +0100

    Release 1.7.0.10 : Monk and barb adaptation by Bombastic

--- Sources/Plugin.cs
+++ Sources/Plugin.cs
@@ -30,7 +30,7 @@ namespace GilesTrinity
         {
             get
             {
-                return new Version(1, 7, 0, 9);
+                return new Version(1, 7, 0, 10);
             }
         }
 
