8afa9b2bc3604086d8644972c7c2c84cc3d222d8
rrrix1 <rrrix@bogusthunder.net>
2014-04-17 09:53:25 -0700

    that was 1.8.24 below :)

--- Trinity/Plugin.cs
+++ Trinity/Plugin.cs
@@ -19,7 +19,7 @@ namespace Trinity
         {
             get
             {
-                return new Version(1, 8, 23);
+                return new Version(1, 8, 24);
             }
         }
 
