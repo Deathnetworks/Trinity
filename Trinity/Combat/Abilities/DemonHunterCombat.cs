52ba0b1e1785bb853f7bbf660828db797585bfa8
rrrix1 <rrrix@bogusthunder.net>
2014-09-22 18:00:14 -0700

    DH Maurader will now cast default attacks instead of just standing

--- Trinity/Combat/Abilities/DemonHunterCombat.cs
+++ Trinity/Combat/Abilities/DemonHunterCombat.cs
@@ -364,7 +364,8 @@ namespace Trinity.Combat.Abilities
                 if (Trinity.ObjectCache.Any(u => u.Type == GObjectType.Unit && u.Weight > 0 && u.RadiusDistance < 15f))
                     return new TrinityPower(SNOPower.Walk, 2f, NavHelper.FindSafeZone(false, 0, Trinity.Player.Position, true));
 
-                return new TrinityPower(SNOPower.Walk);
+                return DefaultPower;
+                //return new TrinityPower(SNOPower.Walk);
             }
 
             return null;
