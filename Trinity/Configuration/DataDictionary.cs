13c5ec9e8d11f767f821f63d133319c40fcb680c
imxzjv <imxzjv@gmail.com>
2014-10-23 11:13:58 +1300

    Two more blacklist SNOs

--- Trinity/Configuration/DataDictionary.cs
+++ Trinity/Configuration/DataDictionary.cs
@@ -1113,7 +1113,9 @@ namespace Trinity
             //x1_Westm_HeroWorship01_VO Dist=10 IsElite=False LoS=True HP=1,00 Dir=SW - Name=x1
             //It's trying to attack that in town
             //x1_Westm_HeroWorship01_VO = 321451,
-            321451, 
+            //x1_Westm_HeroWorship02_VO = 321454,
+            //x1_Westm_HeroWorship03_VO = 321479,
+            321451, 321454, 321479
 
         };
 
