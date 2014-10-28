0d3e16cd0e4c05ebdd646fd135d79dec785726ef
imxzjv <imxzjv@gmail.com>
2014-10-29 10:05:57 +1300

    TrinityCacheObject Serialization

--- Trinity/Cache/TrinityCacheObject.cs
+++ Trinity/Cache/TrinityCacheObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.Serialization;
 using Zeta.Common;
 using Zeta.Game;
 using Zeta.Game.Internals.Actors;
@@ -11,6 +12,7 @@ namespace Trinity
     // Let's me create an object list with ONLY the data I need read from D3 memory, and then read from this while
     // Handling movement and interaction with the target - whether the target is a shrine, an item or a monster
     // Completely minimizing the D3 memory reads to the bare minimum
+    [DataContract]
     public class TrinityCacheObject
     {
         [NoCopy]
@@ -79,88 +81,220 @@ namespace Trinity
             }
         }
 
+        [DataMember]
         public int ACDGuid { get; set; }
+
+        [DataMember]
         public int RActorGuid { get; set; }
+
+        [DataMember]
         public int ActorSNO { get; set; }
 
         // Generic stuff applicable to all objects
+
+        [DataMember]
         public GObjectType Type { get; set; }
+
+        [DataMember]
         public ActorType ActorType { get; set; }
+
+        [DataMember]
         public GizmoType GizmoType { get; set; }
+
+        [DataMember]
         public double Weight { get; set; }
+
+        [DataMember]
         public string WeightInfo { get; set; }
+
+        [DataMember]
         public Vector3 Position { get; set; }
+
+        [DataMember]
         public AABB AABBBounds { get; set; }
+
+        [DataMember]
         public float Distance { get; set; }
+
+        [NoCopy]
         public float RadiusDistance { get { return Math.Max(Distance - Radius, 0f); } }
+
+        [DataMember]
         public string InternalName { get; set; }
+
         public SNOAnim Animation { get; set; }
+
+        [DataMember]
         public int DynamicID { get; set; }
+
+        [DataMember]
         public int GameBalanceID { get; set; }
+
+        [DataMember]
         public int ItemLevel { get; set; }
+
+        [DataMember]
         public string ItemLink { get; set; }
+
+        [DataMember]
         public int GoldAmount { get; set; }
+
+        [DataMember]
         public bool OneHanded { get; set; }
+        
+        [DataMember]
         public bool TwoHanded { get; set; }
+
+        [DataMember]
         public ItemQuality ItemQuality { get; set; }
+
+        [DataMember]
         public ItemBaseType DBItemBaseType { get; set; }
+
+        [DataMember]
         public ItemType DBItemType { get; set; }
+
+        [DataMember]
         public FollowerType FollowerType { get; set; }
+
+        [DataMember]
         public GItemType TrinityItemType { get; set; }
+
+        [DataMember]
         public DateTime LastSeenTime { get; set; }
+
+        [DataMember]
         public bool IsElite { get; set; }
+
+        [DataMember]
         public bool IsRare { get; set; }
+
+        [DataMember]
         public bool IsUnique { get; set; }
+
+        [DataMember]
         public bool IsMinion { get; set; }
+
+        [DataMember]
         public MonsterAffixes MonsterAffixes { get; set; }
+
+        [DataMember]
         public bool IsTreasureGoblin { get; set; }
+
+        [DataMember]
         public bool IsEliteRareUnique { get; set; }
+
+        [DataMember]
         public bool IsBoss { get; set; }
+
+        [DataMember]
         public bool HasAffixShielded { get; set; }
+
+        [DataMember]
         public bool IsAttackable { get; set; }
+
+        [DataMember]
         public bool HasDotDPS { get; set; }
+
+        [DataMember]
         public double HitPointsPct { get; set; }
+
+        [DataMember]
         public double HitPoints { get; set; }
+
+        [DataMember]
         public float Radius { get; set; }
+
+        [DataMember]
         public float Rotation { get; set; }
+
+        [DataMember]
         public Vector2 DirectionVector { get; set; }
+
         /// <summary>
         /// If unit is facing player
         /// </summary>
+        [DataMember]
         public bool IsFacingPlayer { get; set; }
+
         /// <summary>
         /// If Player is facing unit
         /// </summary>
+        [DataMember] 
         public bool ForceLeapAgainst { get; set; }
+
+        [DataMember]
         public bool HasBeenPrimaryTarget { get; set; }
+
+        [DataMember]
         public int TimesBeenPrimaryTarget { get; set; }
+
+        [DataMember]
         public DateTime FirstTargetAssignmentTime { get; set; }
+
+        [DataMember]
         public string ObjectHash { get; set; }
+
+        [DataMember]
         public double KillRange { get; set; }
+
         public MonsterSize MonsterSize { get; set; }
+
+        [DataMember]
         public bool HasBeenNavigable { get; set; }
+
+        [DataMember]
         public bool HasBeenRaycastable { get; set; }
+
+        [DataMember]
         public bool HasBeenInLoS { get; set; }
-        [NoCopy]
+
+        [NoCopy]        
         public bool IsBossOrEliteRareUnique { get { return (this.IsUnit && (IsEliteRareUnique || IsBoss)); } }
+
         [NoCopy]
         public bool IsTrashMob { get { return (this.IsUnit && !(IsEliteRareUnique || IsBoss || IsTreasureGoblin)); } }
+
         [NoCopy]
         public bool IsMe { get { return RActorGuid == Trinity.Player.RActorGuid; } }
+
+        [DataMember]
         public bool IsSummonedByPlayer { get; set; }
+
+        [DataMember]
         public bool IsSummoner { get; set; }
+
+        [DataMember]
         public int SummonedByACDId { get; set; }
+
         [NoCopy]
         public bool IsUnit { get { return this.Type == GObjectType.Unit; } }
+
+        [DataMember]
         public bool IsNPC { get; set; }
+
+        [DataMember]
         public bool NPCIsOperable { get; set; }
+
+        [DataMember]
         public bool IsQuestMonster { get; set; }
+
+        [DataMember]
         public bool IsMinimapActive { get; set; }
+
+        [DataMember]
         public bool IsBountyObjective { get; set; }
+
+        [DataMember]
         public bool IsQuestGiver { get; set; }
 
+        [NoCopy]
         public bool IsCursedChest { get { return Type == GObjectType.CursedChest; } }
+
+        [NoCopy]
         public bool IsCursedShrine { get { return Type == GObjectType.CursedShrine; } }
+
+        [NoCopy]
         public bool IsEventObject { get { return IsCursedChest || IsCursedShrine; } }
 
         [NoCopy]
@@ -260,6 +394,11 @@ namespace Trinity
             }
             catch (Exception) { }
             return false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, Type={1} Dist={2} IsBossOrEliteRareUnique={3} IsAttackable={4}", InternalName, Type, RadiusDistance, IsBossOrEliteRareUnique, IsAttackable);
         }
     }
 }
