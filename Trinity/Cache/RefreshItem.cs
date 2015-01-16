3c2baadeaba2d353440551d2c7e062fd2a2bd582
imxzjv <imxzjv@gmail.com>
2015-01-16 15:53:13 +1300

    Fix for when Drop Legendary option is turned off

--- Trinity/Cache/RefreshItem.cs
+++ Trinity/Cache/RefreshItem.cs
@@ -175,15 +175,19 @@ namespace Trinity
                     }
 
                     // Ignore if item has existed before in this location
-                    if (Settings.Loot.TownRun.DropLegendaryInTown && !CacheData.DroppedItems.Any(i => i.Equals(pickupItem)))
+                    if (Settings.Loot.TownRun.DropLegendaryInTown)
                     {
-                        CacheData.DroppedItems.Add(pickupItem);
-                        AddToCache = true;
-                    }
-                    else
-                    {
-                        Logger.LogDebug("Ignoring Dropped Item = ItemPosition={0} Hashcode={1} DynId={2}", pickupItem.Position, pickupItem.GetHashCode(), pickupItem.DynamicID);
-                        AddToCache = false;
+                        if (!CacheData.DroppedItems.Any(i => i.Equals(pickupItem)))
+                        {
+                            CacheData.DroppedItems.Add(pickupItem);
+                            AddToCache = true;
+                        }
+                        else
+                        {
+                            Logger.LogDebug("Ignoring Dropped Item = ItemPosition={0} Hashcode={1} DynId={2}", pickupItem.Position, pickupItem.GetHashCode(), pickupItem.DynamicID);
+                            AddToCache = false;
+                        }
+                            
                     }
 
                     CacheData.PickupItem.Add(CurrentCacheObject.RActorGuid, AddToCache);
